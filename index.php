781db4e52699259e066fa8cf3a76b9815fb90484
Gilberto Leon <gilberto@ripfla.me>
2014-03-29 04:21:18 -0600

    Algoritmo que regresa las materias reprobadas (avance)

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<?php 
+<?php
 
 require 'class.mysql.php';
 require 'class.rest.php';
 
 require 'helpers.php';
 
-$mysql = new MySQL("tutorvirtual", "root", "");
+$mysql = new MySQL("tutorvirtual", "root", "JoseCuervo");
 
-$query = $mysql->ExecuteSQL("SELECT 
+$query = $mysql->ExecuteSQL("SELECT
     a.`id`
     , a.`nombre`
     , k.`tipo`
     , k.`situacion`
-    , k.`periodo` 
-    FROM kardex AS k 
+    , k.`periodo`
+    FROM kardex AS k
     LEFT JOIN asignatura AS a ON a.`id` = k.`id_asignatura`
     WHERE k.`matricula` = 12216317
     Order by rand()");
@@ -22,94 +22,61 @@ if ($query) {
     if (is_bool($query)) {
         echo "El alumno no ha cursado materias.";
     }else{
+        // prettyArray($query);
+        // Aqui ya tengo todas las materias cursadas
 
+        $failedSubjects = array();
+        foreach($query as $key => $value) {
+        if ($value['situacion'] == 0) {
+          $failedSubjects[$key] = $value;
+        }
+        }
+        // Aqui ya tengo todas las materias reprobadas sin contar las que se aprobaron luego
 
-        usort($query, "usortSituacion");
+        // prettyArray($failedSubjects);
 
-        $hashmap = array();
+        $passedSubjects = array();
+        foreach($query as $key => $value) {
+        if ($value['situacion'] == 1) {
+          $passedSubjects[$key] = $value;
+        }
+        }
+        //Aqui ya tengo las materias aprobadas
+        // prettyArray($passedSubjects);
 
+        foreach($failedSubjects as $key => $value) {
+          foreach($passedSubjects as $key2 => $value2){
+              if ($value2['nombre'] == $value['nombre']){
+                  unset($failedSubjects[$key]);
+              }
+          }
+        }
+        // Aqui ya tengo las materias reprobadas
+        prettyArray($failedSubjects);
 
-        foreach ($query as $key => $value) {
+        $results = array();
 
+        foreach($failedSubjects as $entry){
+            $name = $entry['nombre'];
+            $results[$name]['nombre'] = $name;
 
-            if (!isset($hashmap[$value['nombre']])) {
-
-                $hashmap[$value['nombre']] = $value;
-
-            }elseif ($value['situacion'] == 1 && isset($hashmap[$value['nombre']])) {
-
-                $hashmap[$value['nombre']] = $value;
-
-            }else{
-
-                $periodoAnterior = explode("-", $hashmap[$value['nombre']]['periodo']);
-
-                $periodoActual = explode("-", $value['periodo']);
-
-
-                if ($periodoActual[0] > $periodoAnterior[0]) {
-
-                    if (isset($hashmap[$value['nombre']]['periodosPasados'])) {
-
-                       $value['periodosPasados'] = $hashmap[$value['nombre']]['periodosPasados'];
-
-                    }
-
-                    $value['periodosPasados'][] = implode("-", $periodoAnterior);
-
-                    $hashmap[$value['nombre']] = $value;
-
-                }else if($periodoActual[0] == $periodoAnterior[0]){
-
-                    if ($periodoActual[1] == "ene" && $periodoAnterior[1] == "ago") {
-
-                        $hashmap[$value['nombre']]['periodosPasados'][] = implode("-", $periodoActual);
-
-                    }elseif ($periodoActual[1] == "ago" && $periodoAnterior[1] == "ago") {
-
-                        $hashmap[$value['nombre']]['periodosPasados'][] = implode("-", $periodoActual);
-
-                    }elseif ($periodoActual[1] == "ene" && $periodoAnterior[1] == "ene") {
-
-                        $hashmap[$value['nombre']]['periodosPasados'][] = implode("-", $periodoActual);
-
-                    }elseif ($periodoActual[1] == "ago" && $periodoAnterior[1] == "ene") {
-
-                        if (isset($hashmap[$value['nombre']]['periodosPasados'])) {
-
-                            $value['periodosPasados'] = $hashmap[$value['nombre']]['periodosPasados'];
-
-                        }
-
-                        $value['periodosPasados'][] = implode("-", $periodoAnterior);
-
-                        $hashmap[$value['nombre']] = $value;
-                        
-
-                    }
-
-                }else if($periodoActual[0] < $periodoAnterior[0]){
-
-                    $hashmap[$value['nombre']]['periodosPasados'][] = implode("-", $periodoActual);
-
-                }
-
+            if (isset($results[$name][$entry['periodo']])){
+                $results[$name]['periodos'][] += $entry['periodo'];
+            } else {
+                $results[$name]['periodos'][] = $entry['periodo'];
             }
+        }
+
+        $results = array_values($results);
+
+        prettyArray($results);
 
         }
 
-        foreach ($hashmap as $key => $value) {
-
-            if ($value['situacion'] == 1) {
-                unset($hashmap[$key]);
-            }
-
-        }
-
-        prettyArray($hashmap);
+        // prettyArray($result);
 
     }
-}
+
 
 
 ?>
